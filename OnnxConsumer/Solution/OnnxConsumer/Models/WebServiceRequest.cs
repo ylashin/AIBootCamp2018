1950531a08d5332951f33a3341cffbf56cb63346
Yousry Mohamed <ylashin@gmail.com>
2018-12-15 18:19:34 +1000

    Source code for ONNX model consumer

---
+++ OnnxConsumer/Solution/OnnxConsumer/Models/WebServiceRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace OnnxConsumer.Models
+{
+    public class WebServiceRequest
+    {
+        // List of sample input images, in our case one image in every request
+        // Every image has 3 color channels
+        // Every color channel has list of columns 
+        // Every column has a list of row pixels and this is the final dimension that has pixel values
+
+        public List<List<List<List<int>>>> data { get; set; }
+    }
+
+}
